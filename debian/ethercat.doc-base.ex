75ced2e0a83c50c6c67e8e599007e51bc8ee30cb
Rin Iwai <rin@eyrin.jp>
2023-08-18 20:16:16 +0900

    dh_make

---
+++ debian/ethercat.doc-base.ex
@@ -0,0 +1,20 @@
+Document: ethercat
+Title: Debian ethercat Manual
+Author: <insert document author here>
+Abstract: This manual describes what ethercat is
+ and how it can be used to
+ manage online manuals on Debian systems.
+Section: unknown
+
+Format: debiandoc-sgml
+Files: /usr/share/doc/ethercat/ethercat.sgml.gz
+
+Format: postscript
+Files: /usr/share/doc/ethercat/ethercat.ps.gz
+
+Format: text
+Files: /usr/share/doc/ethercat/ethercat.text.gz
+
+Format: HTML
+Index: /usr/share/doc/ethercat/html/index.html
+Files: /usr/share/doc/ethercat/html/*.html
